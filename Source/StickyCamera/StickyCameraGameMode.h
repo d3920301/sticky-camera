bfc0e2e2e49fc603c2b3072db276182283e09df1
d3920301 <d3920301@live.tees.ac.uk>
2024-11-12 01:30:54 +0000

    Create project using 3rd person template

---
+++ Source/StickyCamera/StickyCameraGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StickyCameraGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStickyCameraGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AStickyCameraGameMode();
+};
+
+
+
